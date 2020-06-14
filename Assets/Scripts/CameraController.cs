3c80cbc693a7bf25e0d2741dac3e3b27b7d235e7
Bjørn Vinther <g.bvinther@gmail.com>
2020-06-14 09:44:03 +0200

    Added car package

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
     public Vector3 positionOffset;
     public Vector3 lookOffset;
     public float stiffness = 0.1f;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        var targetPoint = target.TransformPoint(positionOffset);
-        transform.Translate((targetPoint - transform.position) * stiffness, Space.World);
-        
-        targetPoint = target.TransformPoint(lookOffset);
-        transform.LookAt(targetPoint);
+        target = GameObject.FindGameObjectWithTag("Player")?.transform;
+        if (target == null)
+            Debug.LogError("Camera could not find target");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            var targetPoint = target.TransformPoint(positionOffset);
+            transform.Translate((targetPoint - transform.position) * stiffness, Space.World);
+
+            targetPoint = target.TransformPoint(lookOffset);
+            transform.LookAt(targetPoint);
+        }
     }
 }
